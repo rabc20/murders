241537bc7faa471eb1ff96b21744e264a9e7dffd
Romulo Betancourt <romulo_b@hotmail.com>
2020-05-28 12:28:25 -0500

    first commit. adding readme.txt file just to get started

--- wrangler-data.R
+++ wrangler-data.R
@@ -2,3 +2,4 @@ library(tidyverse)
 murders <- read_csv("data/murders.csv")
 murders <- murders %>% mutate (region=factor(region), rate = total/population*10^5)
 save(murders, file = "rda/murders.rda")
+#cacaca
